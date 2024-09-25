2752bca73ba7130514003c7575229c8b8db8bfa8
f4rral <f4rral@gmail.com>
2024-09-25 15:06:44 +0300

    EncryptFragment

--- app/src/main/java/com/example/secretmessage/MessageFragment.kt
+++ app/src/main/java/com/example/secretmessage/MessageFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
 
 class MessageFragment : Fragment() {
     override fun onCreateView(
@@ -12,7 +14,13 @@ class MessageFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Заполнение макета для этого фрагмента
-        return inflater.inflate(R.layout.fragment_message, container, false)
+        val view = inflater.inflate(R.layout.fragment_message, container, false)
+        val nextButton = view.findViewById<Button>(R.id.next)
+
+        nextButton.setOnClickListener {
+            view.findNavController().navigate(R.id.action_messageFragment_to_encryptFragment)
+        }
+
+        return view
     }
 }
